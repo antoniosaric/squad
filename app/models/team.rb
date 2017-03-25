0ab49dc46c1881820b9cb71109c00c07e231cb14
SalasC2 <christiansalas96@gmail.com>
2017-03-25 16:36:56 -0700

    add associations for users and events

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,8 @@
 class Team < ApplicationRecord
-  belongs_to :user
-  has_many :teammates, through :user
+  has_many :team_users
+  has_many :users, through: :team_users
+  # belongs_to :team_captain, class_name: "User", foreign_key: "user_id"
+  # accepts_nested_attributes_for :team_captain
+  has_many :event_teams
+  has_many :events, through: :event_teams
 end
