e3139482f4bb67d7273c7ec48d05e06c86234821
SalasC2 <christiansalas96@gmail.com>
2017-03-25 18:57:35 -0700

    Add routes for user controller

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -6,7 +6,7 @@ class UserController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save
+    if @user.save 
       redirect_to "/users/#{@user.id}"
     else
       @user.errors.full_messages
