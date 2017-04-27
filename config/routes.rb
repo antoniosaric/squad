9cb6a993a3f2d635138c3e366779edd77d046d56
Tony <saric.tony@gmail.com>
2017-04-27 13:59:43 -0700

    attempts to correct oauth

--- config/routes.rb
+++ config/routes.rb
@@ -5,16 +5,11 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: "omniauth_callbacks" }
 
   resources :users do
-    resources :teams
   end
 
-  
+
   post "/search" => "home#search"
   get "/results" => "home#results"
-
-  get '/users/new' => "user#new"
-  post '/users' => "user#create"
-  get '/users/:id' => "user#profile"
 
 
 end
