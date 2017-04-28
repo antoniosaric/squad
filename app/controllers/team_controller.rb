26472502e0575be3d2c9180b7174b9e4422d7a08
Tony <saric.tony@gmail.com>
2017-04-27 17:31:14 -0700

    rearranges routes

--- app/controllers/team_controller.rb
+++
@@ -1,6 +0,0 @@
-class TeamController < ApplicationController
-  
-  def profile
-
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'home#index'
+  
+  root 'sessions#new'
 
   devise_for :users, controllers: { omniauth_callbacks: "omniauth_callbacks" }
+  
   resources :sessions
   resources :users
 
