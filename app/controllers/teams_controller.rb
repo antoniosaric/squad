238ce180337d72cc3655895e2f274704d37ed23a
Tony <saric.tony@gmail.com>
2017-04-29 00:46:11 -0700

    saved changes

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,7 +1,7 @@
 class TeamController < ApplicationController
   
   def index
-
+    @team = Team.all
   end
 
   #For google and eventbrite api
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
  
   has_many :team_users
   has_many :teams, through: :team_users
